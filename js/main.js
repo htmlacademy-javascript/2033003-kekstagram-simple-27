9d5f8acc8bbe23370f7331613e632fc49b33de00
Alexander <feliugin@gmail.com>
2022-10-08 20:02:54 +0300

    4.15. Больше деталей
    устранил замечание в функции verifyStringLength
    и выполнил домашнее задание 4.15. Больше деталей

--- js/main.js
+++ js/main.js
@@ -3,20 +3,32 @@ const MIN = 2;
 const MAX = 24;
 const STRING = 'Проверяемая_строка';
 const MAX_STRING_LENGTH = 140;
+const MAX_COUNT = 25;
 
-//the first part of the task
 function returnRandomInteger(min, max) {
-  if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {return NaN;}
-  if(max < min){[min, max] = [max, min];}
+  if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') { return NaN; }
+  if (max < min) { [min, max] = [max, min]; }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 returnRandomInteger(MIN, MAX);
 
-//the second part of the task
 function verifyStringLength(string, maxStringLength) {
-  if( typeof string !== 'string'){return null;}
+  if (typeof string !== 'string' || typeof maxStringLength !== 'number') { return null; }
   return string.length <= maxStringLength || false;
 }
 
 verifyStringLength(STRING, MAX_STRING_LENGTH);
+
+const createDescriptionPhoto = (integer) => ({
+  id: integer,
+  url: `photos/${integer}.jpg`,
+  description: 'фото с отпуска',
+  likes: returnRandomInteger(15, 200),
+  comments: returnRandomInteger(0, 200)
+});
+
+const descriptionPhotos = Array.from({ length: MAX_COUNT }, (_element, index) => createDescriptionPhoto(index + 1));
+
+// eslint-disable-next-line no-console
+console.log(descriptionPhotos);
