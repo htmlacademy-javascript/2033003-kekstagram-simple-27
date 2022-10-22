bc04d54387a0df84632752ef5d9e26f0f2886d84
Alexander <feliugin@gmail.com>
2022-10-22 12:18:52 +0300

    удалил лишние комментарии

--- js/photo-rendering.js
+++ js/photo-rendering.js
@@ -13,7 +13,7 @@ const parameters = {
 };
 
 function creatingPhotoElement(data,params) {
-  const pictureContainer = document.querySelector (params.containerClass);//в конце выполнения ошибка по этой строке Uncaught TypeError: Cannot read properties of undefined (reading 'containerClass')
+  const pictureContainer = document.querySelector (params.containerClass);
   const pictureTemplate = document.querySelector (params.templateId).content.querySelector (params.templateClass).cloneNode (true);
   const pictureFragment = document.createDocumentFragment ();
 
@@ -30,37 +30,3 @@ function creatingPhotoElement(data,params) {
 
 creatingPhotoElement(photoData,parameters);
 export { creatingPhotoElement };
-
-/*
-import { createDataPhotos } from './photo-data.js';
-import { MAX_COUNT_OBJECTS } from './CONSTANTS.js';
-
-const params = {
-  containerClass: '.pictures',
-  templateId: '#picture',
-  templateClass: '.picture',
-  urlClass: '.picture__img',
-  likesClass: '.picture__likes',
-  commentsClass: '.picture__comments',
-};
-
-const photosData = createDataPhotos(MAX_COUNT_OBJECTS);
-
-const creatingPhotoElement = (photoData, parameters) => {
-  const photoContainer = document.querySelector (parameters.containerClass);
-  const pictureTemplate = document.querySelector (parameters.templateId).content.querySelector (parameters.templateClass).cloneNode (true); //2
-  const photoFragment = document.createDocumentFragment ();
-
-  photoData.forEach (({url, likes, comments}) => {
-    const photoElement = structuredClone (photoFragment); //3
-    photoElement.querySelector (params.urlClass).url = url;
-    photoElement.querySelector (params.likesClass).textContent = likes;
-    photoElement.querySelector (params.commentsClass).textContent = comments.length;
-    photoFragment.append (photoElement);
-  });
-  photoContainer.append(photoFragment);
-};
-
-creatingPhotoElement(photosData,params);
-export { creatingPhotoElement };
-*/
