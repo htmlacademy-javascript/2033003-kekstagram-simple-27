dfce6f651c6368c63f207a3d3b00a8cf64f15905
Alexander <feliugin@gmail.com>
2022-10-27 19:05:28 +0300

    создал дополнительные модули для будущей задачи

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@ import {createDataPhotos} from'./picture/picture-data.js';
 import {creatingPictureElement} from'./picture/picture-rendering.js';
 import {pictureParameters} from'./picture/picture-parameters.js';
 
-import {createEventHandlers} from'./picture_form/picture-form-events.js';
+import {createPictureFormEventHandlers} from'./picture_form/picture-form-events.js';
 import {pictureformParameters} from'./picture_form/picture-form-parameters.js';
 
-import {}
 
 creatingPictureElement(createDataPhotos(),pictureParameters);
 createPictureFormEventHandlers(pictureformParameters);
