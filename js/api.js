5663b62388ef84e3b71bc0fe70f13cd615a7e2c4
Alexander <feliugin@gmail.com>
2022-11-12 10:40:50 +0300

    переделал на async await
    убрал .then

--- js/api.js
+++ js/api.js
@@ -2,49 +2,42 @@ import { showErrorLoadAlert } from './error.js';
 import { unblockSubmitButton, removeOnPictureFormEscKeydown } from './picture_form/picture-form.js';
 import { showSuccessSaveAlert } from './success.js';
 
-async function getPictures() {
-  const response = await fetch('https://27.javascript.pages.academy/kekstagram-simple/data');
-  if (!response.ok) {
-    throw new Error();
+const getData = async (onSuccess) => {
+  try{
+    const response = await fetch('https://27.javascript.pages.academy/kekstagram-simple/data');
+    if (!response.ok) {
+      throw new Error();
+    }
+    const pictures = await response.json();
+    onSuccess(pictures);
+  }catch(error){
+    showErrorLoadAlert();
   }
-  const pictures = await response.json();
-  return pictures;
-}
-
-const getData = (onSuccess) => {
-  getPictures()
-    .then((pictures) => {
-      onSuccess(pictures);
-    })
-    .catch(() => showErrorLoadAlert());
 };
 
-async function sendPictures(onSuccess, onFail, body) {
-  const response = await fetch(
-    'https://27.javascript.pages.academy/kekstagram-simple',
-    {
-      method: 'POST',
-      body,
-    },
-  );
-  if (response.ok) {
-    onSuccess();
-    unblockSubmitButton();
-    showSuccessSaveAlert();
-  } else {
+const sendData = async (onSuccess, onFail, body) => {
+  try {
+    const response = await fetch(
+      'https://27.javascript.pages.academy/kekstagram-simple',
+      {
+        method: 'POST',
+        body,
+      },
+    );
+    if (response.ok) {
+      onSuccess();
+      unblockSubmitButton();
+      showSuccessSaveAlert();
+    } else {
+      onFail();
+      unblockSubmitButton();
+      removeOnPictureFormEscKeydown();
+    }
+  } catch (error) {
     onFail();
     unblockSubmitButton();
     removeOnPictureFormEscKeydown();
   }
-}
-
-const sendData = (onSuccess, onFail, body) => {
-  sendPictures(onSuccess, onFail, body)
-    .catch(() => {
-      onFail();
-      unblockSubmitButton();
-      removeOnPictureFormEscKeydown();
-    });
 };
 
 export { getData, sendData };
