77c732cd1ffa1a4c0a5822a1ae26765da4ed721c
Alexander <feliugin@gmail.com>
2022-11-17 19:11:34 +0300

    комит

--- js/util/util.js
+++ js/util/util.js
@@ -9,6 +9,7 @@ const verifyStringLength = (string, minStringLength, maxStringLength) => {
   return string.length >= minStringLength && string.length <= maxStringLength || false;
 };
 
+
 const getRandomArrayElement = (elements) => elements[Math.floor(Math.random() * elements.length)];
 const isEscapeKey = (evt) => evt.key === 'Escape';
 const isOutOfBoundClick = (evt, element) => evt.composedPath().includes(document.querySelector(element));
