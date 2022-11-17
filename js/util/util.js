3c0f74a92c033a16dd738ba56642ec6fd72e3a76
Alexander <feliugin@gmail.com>
2022-11-17 17:38:44 +0300

    util

--- js/util/util.js
+++ js/util/util.js
@@ -10,6 +10,8 @@ const verifyStringLength = (string, minStringLength, maxStringLength) => {
 };
 
 const getRandomArrayElement = (elements) => elements[Math.floor(Math.random() * elements.length)];
+const isEscapeKey = (evt) => evt.key === 'Escape';
+const isOutOfBoundClick = (evt, element) => evt.composedPath().includes(document.querySelector(element));
 
 const addEventListeners = (elements) => {
   elements.forEach((element) => {
@@ -34,9 +36,6 @@ const removeClassFromElement = (parameter, className) => {
   htmlElement.classList.remove(className);
 };
 
-const isEscapeKey = (evt) => evt.key === 'Escape';
-
-const isOutOfBoundClick = (evt, element) => evt.composedPath().includes(document.querySelector(element));
 
 export {
   returnRandomInteger, verifyStringLength, getRandomArrayElement,
